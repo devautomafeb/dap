c88ad03a96ead7576ea2c15e33e776c04a5b67fe
Bruno Elyezer Fonseca <brunoelyezerfonseca@MacBook-Air-de-Bruno.local>
2024-08-18 19:55:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h3 className="custom-text">Coming soon...</h3>
+      <div>
+        <h1> </h1>
+      </div>
+      <h1 className="custom-text">dap.</h1>
+    </div>
+  );
+}
+
+export default App;
+
