62d08405c1d5e8f5ba675953610112d704f06acb
devautomafeb@gmail.com <devautomafeb@gmail.com>
2024-08-30 22:37:23 -0300

    7 commit

--- src/components/WhatsButton/index.tsx
+++ src/components/WhatsButton/index.tsx
@@ -1,18 +1,21 @@
-import { FaWhatsapp } from "react-icons/fa"
-
+import { FaWhatsapp } from "react-icons/fa";
 import { FontH1, WhatsappContainer } from "./style";
 
 function WhatsButton() {
     return (
         <div>
-            <a href='https://wa.me/message/5521974569595'>
+            <a 
+                href='https://wa.me/5521974569595'
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label='Send a message on WhatsApp'
+            >
                 <WhatsappContainer>
-
                     <FontH1><FaWhatsapp /></FontH1>
                 </WhatsappContainer>
             </a>
         </div>
-    )
+    );
 }
 
-export default WhatsButton
+export default WhatsButton;
